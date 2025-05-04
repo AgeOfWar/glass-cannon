a48728cc543e8705b327006222b9688593486a62
Michi Palazzo <palazzomichi@gmail.com>
2025-05-04 21:10:47 +0200

    Fix content type middlewares

--- packages/router/src/middleware/contentType.ts
+++ packages/router/src/middleware/contentType.ts
@@ -24,7 +24,12 @@ export function textBody(options?: ContentTypeOptions): Middleware<{ text: strin
     if (!contentType.startsWith('text/plain')) return await onInvalidContentType(context);
     const charsetMatch = /charset=([^;]*)/i.exec(contentType);
     const encoding = (charsetMatch?.[1]?.trim().toLowerCase() ?? 'utf-8') as BufferEncoding;
-    const text = await readText(context.body, encoding);
+    let text: string;
+    try {
+      text = await readText(context.body, encoding);
+    } catch {
+      return await onInvalidContentType(context);
+    }
     return await handler({ text });
   };
 }
@@ -44,7 +49,12 @@ export function jsonBody(options?: ContentTypeOptions): Middleware<{ json: unkno
     }
 
     if (!contentType.startsWith('application/json')) return await onInvalidContentType(context);
-    const json = await readJson(context.body, 'utf-8');
+    let json: unknown;
+    try {
+      json = await readJson(context.body, 'utf-8');
+    } catch {
+      return await onInvalidContentType(context);
+    }
     return await handler({ json });
   };
 }
